5063ea9b2050053e0c09de69b68332873018d7f9
Fumisani2019 <fumza89@gmail.com>
2023-11-12 09:48:10 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,49 @@
 #include <stdarg.h>
 #include "main.h"
+/**
+ * _printf - Reveives the main string and all the necessary parameters to
+ * print a formated string
+ * @format A string containing the needed characters
+ * Returns: A total count of the characters printed
+ */
 int _printf(const char *format, ...)
 {
-    unsigned int i;
-    unsigned int c;
-    va_list args;
+	unsigned int i;
+	unsigned int c;
+	va_list args;
 
-   va_start (args, format);
-   i = 0;
-   c = 0;
+	va_start(args, format);
+	i = 0;
+	c = 0;
 
-   while(format[i] != '\0')
-   {
-    if (format[i] != '%')
-    {
-        ft_putchar(format[i]);
-    }
-    else if (format[i + 1] == 'c')
-    {
-        ft_putchar(va_arg(args,int));
-        i++;
-    }
-    else if (format[i + 1] == 's')
-    {
-        ft_putstr(va_arg(args, char*));
-        i++;
-    }
-    else if (format[i + 1] == '%')
-    {
-        ft_putchar('%');
-    }
-    else if (format[i + 1] == 'd')
-    {
-        ft_putnbr(va_arg(args,int));
-        i++;
-    }
-    c++;
-    i++;
-    va_end(args);
-   }
-   return(c);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			ft_putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			ft_putchar(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			ft_putstr(va_arg(args, char*));
+			i++;
+		}
+		else if (format[i + 1] == '%')
+		{
+			ft_putchar('%');
+		}
+		else if (format[i + 1] == 'd')
+		{
+			ft_putnbr(va_arg(args, int));
+			i++;
+		}
+		c++;
+		i++;
+		va_end(args);
+	}
+	return (c);
 }
--- main.c
+++ main.c
@@ -32,15 +32,13 @@ int main(void)
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    _printf("String:[%s] ==> [%s]\n", "I am a string !", "i m the seconde string");
-    printf("String:[%s] ==> [%s]\n", "I am a string !", "i m the seconde string");
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    // _printf("Unknown:[%r]\n");
-    // printf("Unknown:[%r]\n");
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
